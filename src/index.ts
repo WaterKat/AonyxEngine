df3bcdfcbfb8bce72e7f8a05c0ab1345b92ed989
WaterKat <waterkat@aonyxlimited.com>
2025-04-18 14:30:36 -0700

    bugfix(routers): removing broken import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import express from "express";
 import 'dotenv/config';
 
-import { twitchAuthRouter } from './twitch-utils.js';
 import statusRouter from "./utils-router.js";
-import authRouter from "./auth-router.js";
+import { authRouter } from "./auth-router.js";
 
 
 //vars
@@ -19,7 +18,6 @@ const router = express.Router();
 app.use('/api/v1', router);
 app.use(statusRouter);
 app.use(authRouter);
-app.use(twitchAuthRouter);
 
 
 //MARK: LISTEN
