805e3a9d2c14a32a0521fa99af04c43629c6bef6
WaterKat <waterkat@aonyxlimited.com>
2025-04-22 16:00:23 -0700

    feat(utils): added requireEnvAs function to safely retrieve and parse environment variables

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,51 @@
 import { type CipherGCMTypes, type CipherKey, randomBytes, createCipheriv, createDecipheriv } from 'crypto';
 
 
+// MARK: ENVIRONMENT
+export function requireEnvAs<K extends keyof typeof process.env>(type: 'string', key: K, fallback?: string): string;
+export function requireEnvAs<K extends keyof typeof process.env>(type: 'number', key: K, fallback?: number): number;
+export function requireEnvAs<K extends keyof typeof process.env>(type: 'boolean', key: K, fallback?: boolean): boolean;
+export function requireEnvAs<K extends keyof typeof process.env>(type: 'string' | 'number' | 'boolean', key: K, fallback?: string | number | boolean): string | number | boolean {
+    const raw = process.env[key];
+    const value = raw?.trim();
+
+    if (!value) {
+        if (fallback !== undefined) {
+            return fallback;
+        }
+        throw new Error(`Missing required environmental variable: "${key}".`);
+    }
+
+    switch (type) {
+        case 'string':
+            return value;
+        case 'number': {
+            const num = Number(value);
+            if (!isNaN(num)) return num;
+            if (fallback !== undefined) {
+                console.warn(`Invalid number for "${key}". Using fallback value: "${fallback}".`);
+                return fallback;
+            }
+            throw new Error(`Environment variable "${key}" must be a valid number.`);
+        }
+        case 'boolean': {
+            const bool = value.toLowerCase();
+            if (bool === 'true') return true;
+            if (bool === 'false') return false;
+            if (fallback !== undefined) {
+                console.warn(`Invalid boolean for "${key}". Using fallback value: "${fallback}".`);
+                return fallback;
+            }
+            throw new Error(`Environment variable "${key}" must be "true" or "false".`);
+        }
+        default:
+            throw new Error(`Unknown type "${type}" for environmental variable: "${key}".`);
+    }
+}
+export function isDev(): boolean {
+    return process.env.NODE_ENV === 'development';
+}
+
 // MARK: SAFELY
 type SafelyResultSuccess<T> = { data: T; error: undefined; };
 type SafelyResultFailure = { data: undefined; error: Error; };
