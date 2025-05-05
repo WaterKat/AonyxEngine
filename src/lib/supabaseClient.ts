03ee442014a270e7d2d8a8d5a1a699b489683a0c
WaterKat <waterkat@aonyxlimited.com>
2025-05-05 03:40:45 -0700

    chore(types): adding type definitions generated from supabase utility

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,16 @@
 import 'dotenv/config';
 import { createClient } from '@supabase/supabase-js';
 import { requireEnvAs } from './requireEnvAs.utils.js';
+import { Database as DatabasePrivate } from '../types/database.private.types.js';
+import { Database as DatabasePublic } from '../types/database.public.types.js';
 
 export const supabaseUrl = requireEnvAs('string', 'SUPABASE_URL');
 export const supabaseKey = requireEnvAs('string', 'SUPABASE_SERVICE_KEY');
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
+export const supabasePrivate = createClient<DatabasePrivate>(supabaseUrl, supabaseKey, {
     db: { schema: 'private' }
+});
+
+export const supabasePublic = createClient<DatabasePublic>(supabaseUrl, supabaseKey, {
+    db: { schema: 'public' }
 });
