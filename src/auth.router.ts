1a1046633bfda268df523140907d9774635415fb
WaterKat <waterkat@aonyxlimited.com>
2025-05-06 04:20:49 -0700

    feat(token-validation): mass rewrite of token handling using StateManager and token validation same validation, but now conforms to new provided_user_id column in oauth_tokens table

--- src/auth.router.ts
+++ src/auth.router.ts
@@ -1,25 +1,98 @@
 import 'dotenv/config';
 import { Router } from 'express';
-import { randomBytes } from 'crypto';
 import { type Response } from 'express-serve-static-core';
 import jwt from 'jsonwebtoken';
-import { arrayIsEqual, encryptToken, isDev } from './lib/utils.js';
-import { safelyRun, safelyRunAsync } from './lib/safely.utils.js';
+import { arrayIsEqual, isDev } from './lib/utils.js';
+import { SafelyResult, safelyRun, safelyRunAsync, safelyWrapError } from './lib/safely.utils.js';
 import { requireEnvAs } from './lib/requireEnvAs.utils.js';
 import { supabasePrivate } from './lib/supabaseClient.js';
+import { TwitchAuthTokenResponse, TwitchAuthTokenResponseSuccess as AuthTokenResponseSuccess } from './types/twitch.types.js';
+import { StateManager } from './lib/StateManager.service.js';
+import { TokenData, TokenManager } from './lib/TokenManager.service.js';
 
 
+//MARK: MOCK USER
 const supabaseJWTSecret = isDev() ? requireEnvAs('string', 'SUPABASE_JWT_SECRET', 'dev_jwt_secret') : '';
 const supabaseMockUserId = isDev() ? requireEnvAs('string', 'SUPABASE_MOCK_USER_ID', 'dev_mock_user_id') : '';
 const supabaseMockUserEmail = isDev() ? requireEnvAs('string', 'SUPABASE_MOCK_USER_EMAIL', 'dev_mock_user_email') : '';
+const generateMockUser = () => {
+    console.warn('GENERATING MOCK USER, THIS IS JUST FOR DEVELOPMENT PURPOSES');
+    return jwt.sign({
+        sub: supabaseMockUserId,
+        email: supabaseMockUserEmail,
+        role: 'authenticated',
+        exp: Math.floor(Date.now() / 1000) + 60 * 60,
+        aud: "authenticated",
+        app_metadata: {
+            provider: "email",
+            providers: ["email"],
+        },
+        user_metadata: {
+            full_name: "AonyxEngine"
+        }
+    }, supabaseJWTSecret, { algorithm: 'HS256' });
+}
+const MOCK_USER_JWT = isDev() ? generateMockUser() : '';
 
-const aonyxengineSecretAlgorithm = 'aes-256-gcm';
-const aonyxEngineSecretKey = requireEnvAs('string', 'AONYXENGINE_SECRET_KEY');
+
+//MARK: UTILS
+function redirectHTML(message: string, timeoutInSeconds: number, redirect: string = "/") {
+    message = message
+    return `
+    <!DOCTYPE html>
+    <html>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Redirecting...</title>
+    <head>
+        <script>setTimeout(() => { window.location.replace("${redirect}") }, 1000 * ${timeoutInSeconds});</script>
+    </head>
+    <body>
+        <div>${message}</div>
+        <br>
+        <p>You will be redirected in ${timeoutInSeconds} seconds...</p>
+    </body>
+    </html>
+    `;
+}
+
+function supabaseToSafelyResult<T>(result: { data: T, error: any }): SafelyResult<T> {
+    if (result.error)
+        return { ok: false, error: result.error };
+    return {
+        ok: true,
+        data: result.data
+    }
+}
+
+function parseCodeURL(query: any): SafelyResult<{ code: string, state: string, scopes: string }> {
+    if (!query.code || !query.state) {
+        if (query.error)
+            return { ok: false, error: new Error([query.error, query.error_description].join(' ')) };
+        return { ok: false, error: new Error('code / state not found') };
+    }
+
+    return {
+        ok: true,
+        data: { code: query.code, state: query.state, scopes: query.scope }
+    }
+}
+
+function createRedirectResponseFunction(res: Response, endpoint: string, public_message: string, options?: { code?: number, redirect?: string, timeout_in_seconds?: number, logType?: 'log' | 'info' | 'warn' | 'error' }) {
+    return (reason: string, ...args: any[]) => {
+        console[options?.logType ?? 'log'](`[${new Date().toISOString()}] ${endpoint} ${reason}`, ...args);
+        return res.status(options?.code ?? 200).send(redirectHTML(public_message, options?.timeout_in_seconds ?? 5, options?.redirect));
+    };
+}
+
+
+//MARK: SETUP
 
 const PROVIDER_MAP = {
     twitch: {
         code_endpoint: requireEnvAs('string', 'TWITCH_CODE_ENDPOINT', 'https://id.twitch.tv/oauth2/authorize'),
         token_endpoint: requireEnvAs('string', 'TWITCH_TOKEN_ENDPOINT', 'https://id.twitch.tv/oauth2/token'),
+        validate_endpoint: requireEnvAs('string', 'TWITCH_VERIFY_ENDPOINT', 'https://id.twitch.tv/oauth2/validate'),
         client_id: requireEnvAs('string', 'TWITCH_CLIENT_ID'),
         client_secret: requireEnvAs('string', 'TWITCH_CLIENT_SECRET'),
         redirect_uri: requireEnvAs('string', 'TWITCH_REDIRECT_URL'),
@@ -34,114 +107,104 @@ const PROVIDER_MAP = {
             'channel:read:hype_train',
             'channel:read:redemptions'
         ],
+        verifyToken: (token: string) => safelyRunAsync(() => fetch(PROVIDER_MAP.twitch.validate_endpoint, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(res => res.json()).then(data => ({ token: token, provider_user_id: data.user_id }))),
     },
     discord: {
         code_endpoint: requireEnvAs('string', 'DISCORD_CODE_ENDPOINT', 'https://discord.com/api/oauth2/authorize'),
         token_endpoint: requireEnvAs('string', 'DISCORD_TOKEN_ENDPOINT', 'https://discord.com/api/oauth2/token'),
+        validate_endpoint: requireEnvAs('string', 'DISCORD_VERIFY_ENDPOINT', 'https://discord.com/api/users/@me'),
         client_id: requireEnvAs('string', 'DISCORD_CLIENT_ID'),
         client_secret: requireEnvAs('string', 'DISCORD_CLIENT_SECRET'),
         redirect_uri: requireEnvAs('string', 'DISCORD_REDIRECT_URL'),
         scopes: [],
+        verifyToken: (token: string) => safelyRunAsync(() => fetch(PROVIDER_MAP.discord.validate_endpoint, {
+            method: 'PATCH',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        }).then(res => res.json()).then(data => ({ token: token, provider_user_id: data.id }))),
     }
 } as Record<string, {
     code_endpoint: string,
     token_endpoint: string,
+    validate_endpoint: string,
     client_id: string,
     client_secret: string,
     redirect_uri: string,
     scopes: string[],
+    verifyToken: (token: string) => Promise<SafelyResult<TokenData>>
 }>;
 
 
-//MARK: UTILS
-function redirectHTML(message: string, timeoutInSeconds: number, redirect: string = "/") {
-    message = message
-    return `
-    <!DOCTYPE html>
-    <html>
-        <meta charset="utf-8"
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Redirecting...</title>
-    </head>
-        <script>setTimeout(() => { window.location.replace("${redirect}") }, 1000 * ${timeoutInSeconds});</script>
-    </head>
-    <body>
-        <div>${message}</div>
-        <br>
-        <p>You will be redirected in ${timeoutInSeconds} seconds...</p>
-    </body>
-    </html>
-    `;
-}
-
-function createRedirectResponseFunction(res: Response, endpoint: string, public_message: string, options?: { code?: number, redirect?: string, timeout_in_seconds?: number, logType?: 'log' | 'info' | 'warn' | 'error' }) {
-    return (reason: string, ...args: any[]) => {
-        console[options?.logType ?? 'log'](`[${new Date().toISOString()}] ${endpoint} ${reason}`, ...args);
-        return res.status(options?.code ?? 200).send(redirectHTML(public_message, options?.timeout_in_seconds ?? 5, options?.redirect));
-    };
-}
-
-
-//MARK: DATABASE
+//MARK: ROUTER
 const authRouter = Router();
 
 
 //MARK: CALLBACK
 authRouter.get('/auth/v1/callback', async (req, res): Promise<any> => {
-    const success = createRedirectResponseFunction(res, '/auth/v1/twitch/token', 'authentication successful!');
+    const success = createRedirectResponseFunction(res, '/auth/v1/callback', 'authentication successful!');
     const fail = createRedirectResponseFunction(res, '/auth/v1/callback', 'authentication failed', { code: 400, logType: 'error' });
 
-    const { code, scope, error, error_description, state } = req.query;
+    const user_jwt: string = req.header["Authorization"] ? req.header["Authorization"] : isDev() ? MOCK_USER_JWT : '';
 
-    if (error || !code) return fail('code-error', { error, error_description });
+    const user_data = await supabasePrivate.auth.getUser(user_jwt).then(supabaseToSafelyResult);
+    if (user_data.ok === false) return fail('cb-user-error', user_data.error);
 
-    const { data: states_data, error: states_error } = await supabasePrivate.from('oauth_states').select('*').eq('state', state).gt('expires_at', new Date().toISOString()).single();
+    const code_data = parseCodeURL(req.query);
+    if (code_data.ok === false) return fail('cb-code-error', code_data.error);
 
-    if (states_error) return fail('sb-states-error', states_error);
+    const state_data = await StateManager.use(user_data.data.user.id, code_data.data.state);
+    if (state_data.ok === false) return fail('cb-state-error', state_data.error);
 
-    const { user_id, provider, purpose } = states_data;
-    const provider_data = PROVIDER_MAP[provider as string];
+    const provider_data = PROVIDER_MAP[state_data.data.provider];
+    if (!provider_data) return fail('cb-provider-error', state_data.data.provider);
 
-    if (!provider_data) return fail('provider-error', provider);
+    const token_request_body = {
+        client_id: provider_data.client_id,
+        client_secret: provider_data.client_secret,
+        code: code_data.data.code,
+        grant_type: 'authorization_code',
+        redirect_uri: provider_data.redirect_uri
+    };
 
-    const { data: token_req_data, error: token_req_error } = await safelyRunAsync(() => fetch(
+    const token_req: SafelyResult<TwitchAuthTokenResponse> = await safelyRunAsync(() => fetch(
         provider_data.token_endpoint, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded'
         },
-        body: new URLSearchParams({
-            client_id: provider_data.client_id,
-            client_secret: provider_data.client_secret,
-            code: code.toString(),
-            grant_type: 'authorization_code',
-            redirect_uri: provider_data.redirect_uri
-        }).toString()
-    }
-    ).then(response => response.json()));
+        body: new URLSearchParams(token_request_body).toString()
+    }).then(response => response.json()));
 
-    if (token_req_error) return fail('token-req-error', token_req_error);
-    if (!token_req_data.refresh_token) return fail('token-req-error', token_req_data);
+    if (token_req.ok === false) return fail('cb-token-req1-error', token_req.error);
+    if ('error' in token_req.data) return fail('cb-token-req2-error', JSON.stringify(token_req.data));
 
-    const { refresh_token, expires_in, scope: token_scope } = token_req_data;
+    const { access_token, refresh_token, scope } = token_req.data as AuthTokenResponseSuccess;
 
-    if (!arrayIsEqual(scope.toString().split(' ') ?? [], token_scope)) return fail('mismatch-scope', scope.toString().split(' '), token_scope);
+    if (!arrayIsEqual(code_data.data.scopes.toString().split(' ') ?? [], scope)) return fail('mismatch-scope', code_data.data.scopes.toString().split(' '), scope);
 
-    const { error: token_error } = await supabasePrivate.from('oauth_tokens').upsert(
-        {
-            user_id,
-            provider,
-            token: encryptToken(refresh_token, aonyxengineSecretAlgorithm, aonyxEngineSecretKey),
-            token_type: 'refresh_token',
-            purpose,
-            expires_at: new Date(Date.now() + (expires_in * 1000)).toISOString(),
-            metadata: { scope }
-        }
-        , { onConflict: 'user_id, purpose' })
+    const verification_req = await provider_data.verifyToken(access_token);
+    if (verification_req.ok === false) return fail('cb-verify-token-error', verification_req.error);
 
-    if (token_error) return fail('sb-token-error', token_error);
+    const { provider_user_id } = verification_req.data;
 
-    return success(`login by ${user_id}`);
+    const set_refresh_token = await TokenManager.set(user_data.data.user.id, state_data.data.provider, state_data.data.purpose, 'refresh_token', {
+        token: token_req.data.refresh_token,
+        provider_user_id: provider_user_id
+    });
+    if (set_refresh_token.ok === false) return fail('cb-set-refresh-token-error', set_refresh_token.error);
+
+    const set_access_token = await TokenManager.set(user_data.data.user.id, state_data.data.provider, state_data.data.purpose, 'access_token', {
+        token: token_req.data.access_token,
+        provider_user_id: provider_user_id
+    });
+    if (set_access_token.ok === false) return fail('cb-set-access-token-error', set_access_token.error);
+
+    return success(`login by ${user_data.data.user.id}`);
 });
 
 
@@ -164,90 +227,46 @@ authRouter.get('/auth/v1/discord/login', async (req, res) => {
 
 //MARK: TWITCH
 authRouter.get('/auth/v1/twitch/login', async (req, res) => {
-    const { data, error } = await supabasePrivate.auth.signInWithOAuth({
+    const sign_in = await supabasePrivate.auth.signInWithOAuth({
         provider: 'twitch',
+    }).then(supabaseToSafelyResult)
 
-    })
-
-    if (error) {
+    if (sign_in.ok === false) {
         res.status(500).send(redirectHTML("there was an error getting your request", 10));
         return;
     }
 
-    if (data.url)
-        res.redirect(data.url);
+    res.redirect(sign_in.data.url);
 });
 
 authRouter.get('/auth/v1/twitch/token', async (req, res): Promise<any> => {
     const fail = createRedirectResponseFunction(res, '/auth/v1/twitch/token', 'request failed', { code: 400, logType: 'error' });
 
-    let provided_token = req.header["Authorization"];
+    const user_jwt: string = req.header["Authorization"] ? req.header["Authorization"] : isDev() ? MOCK_USER_JWT : '';
 
-    if (isDev() && (!provided_token || provided_token === "")) {
-        console.warn(`[${new Date().toISOString()}] Token not provided. DEVELOPMENT MODE using MOCK USER`);
-        const { data: mock_token, error: mock_token_error } = safelyRun(() => jwt.sign({
-            sub: supabaseMockUserId,
-            email: supabaseMockUserEmail,
-            role: 'authenticated',
-            exp: Math.floor(Date.now() / 1000) + 60 * 60,
-            aud: "authenticated",
-            app_metadata: {
-                provider: "email",
-                providers: ["email"],
-            },
-            user_metadata: {
-                full_name: "AonyxEngine"
-            }
-        }, supabaseJWTSecret, { algorithm: 'HS256' }));
-        if (mock_token_error) {
-            console.error(`[${new Date().toISOString()}] /auth/v1/twitch/token DEVELOPMENT MODE`, mock_token_error);
-        } else {
-            provided_token = mock_token;
-        }
-    }
+    const user_data = await supabasePrivate.auth.getUser(user_jwt).then(supabaseToSafelyResult);
+    if (user_data.ok === false) return fail('sb-user-error', user_data.error);
 
-    const { data: user_data, error: sb_user_error } = await supabasePrivate.auth.getUser(provided_token);
-    const { user } = user_data;
-    const twitch_identity = user?.identities?.find(identity => identity.provider === 'twitch');
+    const state_data = await StateManager.create(user_data.data.user.id, 'twitch', 'chatbot');
+    if (state_data.ok === false) return fail('sb-state-error', state_data.error);
 
-    if (!user || sb_user_error || !twitch_identity) {
-        console.log('anon redirected to login', sb_user_error);
-        return res.redirect('/auth/v1/twitch/login');
-    }
-
-    console.log('user identities', user.identities);
-
-    const timeout_in_minutes = 5;
     const force_verify = req.query['force_verify']?.toString() ?? 'false';
-    const state = randomBytes(16).toString('hex')
 
     const { code_endpoint, client_id, redirect_uri, scopes } = PROVIDER_MAP['twitch'];
 
-    const search_params = new URLSearchParams({
+    const search_params = {
         client_id,
         force_verify,
         redirect_uri,
         response_type: 'code',
         scope: scopes.join(' '),
-        state,
-    }).toString();
+        state: state_data.data.state,
+    };
 
-    const code_request_url = code_endpoint + '?' + search_params;
-
-    const { error } = await supabasePrivate.from('oauth_states').insert([{
-        user_id: user.id,
-        state: state,
-        provider: 'twitch',
-        expires_at: new Date(Date.now() + (1000 * 60 * timeout_in_minutes)).toISOString(),
-        purpose: 'chatbot'
-    }]);
-
-    if (error) return fail('sb-code-error', error);
+    const code_request_url = code_endpoint + '?' + new URLSearchParams(search_params).toString();
 
     return res.redirect(code_request_url);
 });
 
 
-//
 export { authRouter };
-
